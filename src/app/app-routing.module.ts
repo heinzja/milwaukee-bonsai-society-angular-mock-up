0d766bbf7baad1eeed3848c661f0bd975746e256
l0-w-0l <joseph.heinz17@outlook.com>
2021-05-10 17:02:05 -0500

    -- add routing to Home & The Latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './content/home/home.component';
+import { TheLatestComponent } from './content/the-latest/the-latest.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'the-latest', component: TheLatestComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: TheLatestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
+
+  }
+
+  onHome(event: any):void {
+    this.router.navigate(['/home'])
+  }
+
+  onTheLatest(event: any):void {
+    this.router.navigate(['/the-latest'])
   }
 
 }
